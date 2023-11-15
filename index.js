dec92f2fd038b313e93920933e6683b9968cf457
Aarthivvj <140152685+Aarthivvj@users.noreply.github.com>
2023-11-15 16:27:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('You are  in AWS  Cloud TECHESSAY AarthI. Now code is updated Version1.1'); //write a response to the client
+  res.write('You are  in AWS  Cloud TECHESSAY AarthI. Now code is updated Version 1'); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
