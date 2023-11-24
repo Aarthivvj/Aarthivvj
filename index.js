461078bfb8c11f2208ef340ce17818fa378e920f
Aarthivvj <140152685+Aarthivvj@users.noreply.github.com>
2023-11-24 10:03:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('You are  in AWS  AarthI. Now code is updated to Run to Version 2.0 after edit'); //write a response to the client
+  res.write('You are  in AWS  AarthI. Now code is updated to Run to Version 2.0 after edit'); 
   res.end(); //end the response
 }).listen(5020); //the server object listens on port 5020
